f3715e8c5d94faa7c492a3772e3d88b04858dc9a
yassinekamouss <yassinekamouss76@gmail.com>
2026-08-11 05:43:24 +0100

    refactor(js): audit and clean frontend type definitions

--- resources/js/types/index.ts
+++ resources/js/types/index.ts
@@ -2,3 +2,5 @@ export type * from './auth';
 export type * from './navigation';
 export type * from './ui';
 export type * from './registration';
+export type * from './taxonomies';
+export type * from './offre';
