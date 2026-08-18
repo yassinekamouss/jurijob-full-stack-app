35d899f4f4935cfef02d545c05a4fb2a872a3a2d
ahyahya1616 <ahmaneyahya10@gmail.com>
2026-08-18 15:28:24 +0100

    modify Admin/OffreController.pho

--- app/Http/Controllers/Admin/OffreController.php
+++ app/Http/Controllers/Admin/OffreController.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 <?php
 
 namespace App\Http\Controllers\Admin;
@@ -358,4 +356,3 @@ class OffreController extends Controller
         );
     }
 }
->>>>>>> ce3f8cdbfe8335df70bf5921f1ca6f7d2c3e11c5
