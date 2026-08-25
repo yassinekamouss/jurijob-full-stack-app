0b87a1c1b109bee2b2f8aa4853d454f73ab9c119
ahyahya1616 <ahmaneyahya10@gmail.com>
2026-08-26 00:33:32 +0100

    modify console.pho

--- routes/console.php
+++ routes/console.php
@@ -10,6 +10,6 @@ Artisan::command('inspire', function () {
 
 // Envoi quotidien d'un rappel aux candidats (max 210) ayant un profil incomplet
 Schedule::command('app:send-profile-reminders')
-    ->dailyAt('00:25')
+    ->dailyAt('23:15')
     ->timezone('Africa/Casablanca');
 
